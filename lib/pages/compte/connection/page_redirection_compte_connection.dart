75ead4621e41ac1bdae2a1e35206e0b8f53ddc01
toure.dev <toureabdoulaye003@gmail.com>
2025-05-31 14:50:39 +0000

    Suppression des  modifications boutton faceBook

--- lib/pages/compte/connection/page_redirection_compte_connection.dart
+++ lib/pages/compte/connection/page_redirection_compte_connection.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:koontaa/functions/firebase_auth.dart';
 import 'package:koontaa/pages/home/home.dart';
-import 'package:koontaa/pages/home/home_widgets.dart';
+//import 'package:koontaa/pages/home/home_widgets.dart';
 import 'package:koontaa/pages/compte/connection/page_connection.dart';
 
 class RedirectionPageCompteConnection extends StatefulWidget {
